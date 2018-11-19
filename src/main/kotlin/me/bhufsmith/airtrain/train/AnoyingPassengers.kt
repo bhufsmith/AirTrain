e71af5f43c9cd041d62b9c7c199af1cc8093a552
Brian Hufsmith <brian@Brians-MacBook-Pro.local>
2018-11-19 12:49:57 -0500

    Made AnoyingPassenger thread utilize the Randomness class.

--- src/main/kotlin/me/bhufsmith/airtrain/train/AnoyingPassengers.kt
+++ src/main/kotlin/me/bhufsmith/airtrain/train/AnoyingPassengers.kt
@@ -9,18 +9,6 @@ class AnoyingPassengers(private val messageService: TrainMessageService,
                         private val messagesPerHour:Int = 5,
                         private val urgenPerHour:Int = 2): Runnable, MessageReceiver {
 
-    val anoyingMessages = listOf<String>("Are we there yet!!!", "How much longer till my stop?",
-                                        "Do you like driving trains?", "Why is there a squeaking noise in the back?",
-                                        "There is a drunk guy throwing up in the back!!!!!",
-                                        "Why did we stop for so long?", "You really need to fix these lights....",
-                                        "Slow down.... I don't want to get to work yet!", "Did the train break down?",
-                                        "Can you let me off between stops?", "Some guy is breathing on me... can you do something?",
-                                        "WTF, why are there so many people on the train???", "Why is there no one else on the train?",
-                                        "Do you have bottle service?", "Come on... is it really that hard to drive a train?",
-                                        "Can you do something about all these bumps?")
-
-    val passengerNames = listOf("Jake, State-farm", "Your, Mom", "Tom, Clancy", "Judy, Judge", "James, Bond",
-                                "James", "Bill", "Ted", "Brian", "Lauren", "Christen", "Audrey", "Meagan", "Lisa")
 
     private val trainPassengers = mutableListOf<SimpleMessengerUser>()
     private var hourlyUrgent = urgenPerHour
@@ -30,12 +18,11 @@ class AnoyingPassengers(private val messageService: TrainMessageService,
 
         trainPassengers.forEach{ it.unsubscribe( this) }
         trainPassengers.clear()
-        val userOrder = (0 until passengerNames.size).shuffled()
 
         //Select 5 names to use and register the users.
         var messengerUser: SimpleMessengerUser
         for (i in 0 until 5) {
-            messengerUser = messageService.registerUser(passengerNames[userOrder[i]])
+            messengerUser = messageService.registerUser( RandomNess.randomName() )
             trainPassengers.add(messengerUser)
             messengerUser.subscribeForMessages( this )
         }
@@ -43,7 +30,6 @@ class AnoyingPassengers(private val messageService: TrainMessageService,
 
     private fun sendMessagesForHour(){
         hourlyUrgent = urgenPerHour
-        val messageOrder = (0 until anoyingMessages.size).shuffled()
 
         val messageMaxInterval: Int = timeHelper.minPerHour / messagesPerHour
         var messageCountDown = this.messagesPerHour
@@ -59,8 +45,7 @@ class AnoyingPassengers(private val messageService: TrainMessageService,
             minTimeSpent += waitTime
 
             //Since both, passenger list, and messageOrder list are in random order, we can use the same index for both
-            messageIndex = messageOrder[messageCountDown - 1]
-            trainPassengers[ messageCountDown - 1 ].sendMessage( anoyingMessages[ messageIndex])
+            trainPassengers[ messageCountDown - 1 ].sendMessage( RandomNess.randomMessage() )
 
             messageCountDown--
         }
